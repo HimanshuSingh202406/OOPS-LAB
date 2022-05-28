4700bc377fc5078cbe907570613dd3a34b219ada
HimanshuSingh202406 <96714355+HimanshuSingh202406@users.noreply.github.com>
2022-05-28 10:10:04 +0530

    Create Program-18.cpp

---
+++ Program-18.cpp
@@ -0,0 +1,48 @@
+// A program to show the concept of runtime polymorphism using virtual function
+
+#include <iostream>
+using namespace std;
+
+// Declaration of Base class
+class shape {
+public:
+	virtual void calculate()            //virtual function
+	{
+		cout << "Area: ";
+	}
+};
+
+// Declaration of derived class
+class Rectangle : public shape {
+public:
+	int width, length, area;
+
+	void calculate()      //overrideen function from base class 
+	{
+		cout << "Width of Rectangle: ";
+		cin >> width;
+
+		cout << "Length of Rectangle: ";
+		cin >> length;
+
+		area = length * width;
+		cout << "Area of Rectangle: " << area << " units"<<"\n";
+	}
+};
+
+
+int main()
+{
+
+	// base class pointer
+	shape* s;
+	Rectangle r;
+
+	// initilization of reference variable
+	s = &r;
+
+	// calling of Rectangle function
+	s->calculate();
+	
+	return 0;
+}
